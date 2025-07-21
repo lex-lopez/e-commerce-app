f2bf3e2214f02fefb325fa01b7b9bc2934ca2254
lex-lopez <l3xlop@gmail.com>
2025-07-21 15:21:58 -0600

    Adding Entities for Order and OrderItems

---
+++ src/main/java/com/alopez/store/entities/OrderItems.java
@@ -0,0 +1,35 @@
+package com.alopez.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_items")
+public class OrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
